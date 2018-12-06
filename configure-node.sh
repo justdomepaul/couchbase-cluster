7049e7241e6b8a5faf04d3d2781eac52a1a9480b
Maxfocker <max.focker.shih@gmail.com>
2018-12-06 16:54:01 +0800

    modify shell

--- configure-node.sh
+++ configure-node.sh
@@ -29,7 +29,7 @@ echo "Type: $TYPE"
 if [ "$TYPE" = "WORKER" ]; then
   sleep 15
 
-  IP=`hostname -I`
+  IP=`hostname -icd`
 
   echo "Auto Rebalance: $AUTO_REBALANCE"
   if [ "$AUTO_REBALANCE" = "true" ]; then
