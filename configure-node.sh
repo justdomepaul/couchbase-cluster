35eadd8309e3774f0e90830c916572db30e9eadb
Maxfocker <max.focker.shih@gmail.com>
2019-05-18 00:24:30 +0800

    Update configure-node.sh

--- configure-node.sh
+++ configure-node.sh
@@ -6,7 +6,7 @@ set -m
 sleep 15
 
 # Setup index and memory quota
-curl -v -X POST http://127.0.0.1:8091/pools/default -d memoryQuota=1024 -d indexMemoryQuota=512
+curl -v http://127.0.0.1:8091/pools/default -d memoryQuota=1024 -d indexMemoryQuota=512
 
 # Setup services
 curl -v http://127.0.0.1:8091/node/controller/setupServices -d services=kv%2Cn1ql%2Cindex
@@ -15,7 +15,7 @@ curl -v http://127.0.0.1:8091/node/controller/setupServices -d services=kv%2Cn1q
 curl -v http://127.0.0.1:8091/settings/web -d port=8091 -d username=$CLUSTER_CUSTOM_USERNAME -d password=$CLUSTER_CUSTOM_PASSWORD
 
 # Setup Memory Optimized Indexes
-# curl -i -u $CLUSTER_CUSTOM_USERNAME:$CLUSTER_CUSTOM_PASSWORD -X POST http://127.0.0.1:8091/settings/indexes -d 'storageMode=memory_optimized'
+curl -i -u $CLUSTER_CUSTOM_USERNAME:$CLUSTER_CUSTOM_PASSWORD -X POST http://127.0.0.1:8091/settings/indexes -d 'storageMode=forestdb'
 
 
 if [[ "$HOSTNAME" == *-0 ]]; then
